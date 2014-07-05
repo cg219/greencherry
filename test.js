05fe3b5f339d3003ca7af9f03c475cf10606004b
Clemente Gomez <zomegpro@gmail.com>
2014-07-04 23:18:13 -0400

    Refeactoring + Router patches

--- test.js
+++ test.js
@@ -7,5 +7,12 @@ router.add("/", function(req, res){
 	res.end("Server Pinged From Router");
 })
 
+var post = router.add("/posting/:haha", function(req, res){
+	res.writeHead(200);
+	console.log("Posting Page")
+	console.log("Params: " + req.parameters.haha);
+	res.end("Server Pinged From Router");
+})
+
 server.create(router.router);
 server.listen();
